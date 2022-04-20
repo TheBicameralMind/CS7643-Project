1ba76d54eec706153be760ca19c598c6d885c6de
aidan <achaplin3@gatech.edu>
2022-04-19 22:02:51 -0400

    dataset speedup maybe and real* main

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,10 +1,60 @@
+import os
+
 import numpy as np
+from pathlib import Path
 from skimage.exposure import equalize_adapthist
+import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-image_size = (45, 45)
-batch_size = 16
+_data_dir = Path('dataset')
+_train = _data_dir / Path('Train')
+_test = _data_dir / Path('Test')
 
+image_size = (45, 45)
+class_names = np.array(sorted([item.name for item in _test.glob('*')], key=lambda x: int(x)))
+
+
+# def _get_label(file_path) -> np.ndarray:
+#     # Convert the path to a list of path components
+#     parts = tf.strings.split(file_path, os.path.sep)
+#     # The second to last is the class-directory
+#     return parts[-2] == class_names
+#
+#
+# def _decode_img(img):
+#     # Convert the compressed string to a 3D uint8 tensor
+#     img = tf.io.decode_jpeg(img, channels=3)
+#     # Resize the image to the desired size
+#     return tf.image.resize(img, image_size)
+#
+#
+# def _process_path(file_path):
+#     label = _get_label(file_path)
+#     # Load the raw data from the file as a string
+#     img = tf.io.read_file(file_path)
+#     img = _decode_img(img)
+#     return img, label
+#
+#
+# def _configure_for_performance(ds, batch_size):
+#     ds = ds.cache()
+#     ds = ds.shuffle(buffer_size=1000)
+#     ds = ds.batch(batch_size)
+#     ds = ds.prefetch(buffer_size=tf.data.AUTOTUNE)
+#     return ds
+#
+# def _dataset(from_path, batch_size) -> tf.data.Dataset:
+#     ds = tf.data.Dataset.list_files(str(from_path / '*/*'), shuffle=False)
+#     ds = ds.map(_process_path, num_parallel_calls=tf.data.AUTOTUNE)
+#     ds = _configure_for_performance(ds, batch_size)
+#
+#     return ds
+#
+# def train(batch_size=32) -> tf.data.Dataset:
+#     return _dataset(_train, batch_size)
+#
+# def test(batch_size=32) -> tf.data.Dataset:
+#     return _dataset(_test, batch_size)
 
 def _clahe(img: np.ndarray) -> np.ndarray:
     return equalize_adapthist(img, clip_limit=0.1)
@@ -19,7 +69,8 @@ _train_datagen = ImageDataGenerator(
 )
 _test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-train = _train_datagen.flow_from_directory(
+def train(batch_size)  -> tf.data.Dataset:
+    return _train_datagen.flow_from_directory(
     "dataset/Train",
     subset="training",
     seed=666,
@@ -28,7 +79,8 @@ train = _train_datagen.flow_from_directory(
     class_mode="categorical",
 )
 
-val = _train_datagen.flow_from_directory(
+def val(batch_size) -> tf.data.Dataset:
+    return _train_datagen.flow_from_directory(
     "dataset/Train",
     subset="validation",
     seed=666,
@@ -37,7 +89,8 @@ val = _train_datagen.flow_from_directory(
     class_mode="categorical",
 )
 
-test = _test_datagen.flow_from_directory(
+def test(batch_size) -> tf.data.Dataset:
+    return _test_datagen.flow_from_directory(
     "dataset/Test",
     seed=666,
     batch_size=batch_size,
--- main.py
+++ main.py
@@ -1,23 +1,66 @@
 import os
+import shutil
 import dataset
 from phraselabel import DeterministicLabeler
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.layers import *
 from tensorflow.keras import losses, metrics, optimizers as optim
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 
-num_classes = len(dataset.train.class_names)
+num_classes = len(dataset.class_names)
 
 labeler = DeterministicLabeler()
+
+preprocessing = keras.Sequential([
+    Rescaling(scale=1./255),
+    RandomFlip('horizontal'),
+    RandomRotation(0.2),
+    RandomTranslation(0.15, 0.15),
+    # RandomZoom(0.1)
+])
+
 model = keras.Sequential([
-    Conv2D(32, (3, 3), activation='relu'),
-    Conv2D(64, (3, 3), activation='relu'),
-    Conv2D(64, (3, 3), activation='relu'),
+    # preprocessing,
+
+    Conv2D(64, (5, 5), activation='relu', padding='same'),
+    Conv2D(128, (4, 4), activation='relu', padding='same'),
+    Conv2D(256, (4, 4), activation='relu'),
+    BatchNormalization(),
+
+    Conv2D(512, (3, 3), activation='relu'),
+    Conv2D(512, (3, 3), activation='relu'),
+    Conv2D(512, (3, 3), activation='relu'),
     Flatten(),
-    Dense(128, activation='relu'),
+
+    Dense(256, activation='relu'),
     Dense(num_classes, activation='softmax')
 ])
 model._name = labeler(model)
+print(model.name)
+
+shutil.rmtree(f'tensorboard/{model.name}', ignore_errors=True)
+shutil.rmtree(f'checkpoints/{model.name}', ignore_errors=True)
+
+callbacks = [
+    TensorBoard(log_dir=f'tensorboard/{model.name}'),
+    ModelCheckpoint(filepath=f'checkpoints/{model.name}')
+]
+
+metric = [metrics.Accuracy(), metrics.Precision(), metrics.Recall()]
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=metric
+)
+model.fit(
+    dataset.train(128),
+    batch_size=128,
+    epochs=50,
+    validation_data=dataset.val(128),
+    callbacks=callbacks
+)
