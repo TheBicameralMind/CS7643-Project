e28680fc911b6536b9c5911b496b5b03c326309f
Aidan Chaplin <aidan.chaplin@gmail.com>
2022-04-30 21:05:09 -0400

    finished project

--- main.py
+++ main.py
@@ -9,34 +9,31 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.layers import *
 from tensorflow.keras import losses, metrics, optimizers as optim
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
 num_classes = len(dataset.class_names)
 
 labeler = DeterministicLabeler()
 
-preprocessing = keras.Sequential([
-    Rescaling(scale=1./255),
-    # RandomFlip('horizontal'),
-    RandomRotation(0.2),
-    RandomTranslation(0.15, 0.15),
-    RandomZoom(0.1)
-])
-
 model = keras.Sequential([
-    preprocessing,
+    Conv2D(16, (5, 5), activation='relu', padding='same', input_shape=(*dataset.image_size, 3)),
+    Conv2D(32, (4, 4), activation='relu', padding='same'),
+    MaxPooling2D((2, 2)), 
+    Dropout(0.25),
 
-    Conv2D(64, (5, 5), activation='relu', padding='same'),
-    Conv2D(128, (4, 4), activation='relu', padding='same'),
-    Conv2D(256, (4, 4), activation='relu'),
-    BatchNormalization(),
+    Conv2D(32, (3, 3), activation='relu', padding='same'),
+    Conv2D(64, (3, 3), activation='relu', padding='same'),
+    MaxPooling2D((2, 2)), 
+    Dropout(0.25),
 
-    Conv2D(512, (3, 3), activation='relu'),
-    Conv2D(512, (3, 3), activation='relu'),
-    Conv2D(512, (3, 3), activation='relu'),
-    Flatten(),
+    Conv2D(64, (3, 3), activation='relu'),
+    Conv2D(32, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)), 
+    Dropout(0.25),
 
-    Dense(256, activation='relu'),
+    Flatten(), 
+    Dense(64, activation='relu'),
+    Dropout(0.25), 
     Dense(num_classes, activation='softmax')
 ])
 model._name = labeler(model)
@@ -47,22 +44,31 @@ shutil.rmtree(f'checkpoints/{model.name}', ignore_errors=True)
 
 callbacks = [
     TensorBoard(log_dir=f'tensorboard/{model.name}'),
-    # ModelCheckpoint(filepath=f'checkpoints/{model.name}')
+    ModelCheckpoint(filepath=f'checkpoints/{model.name}', save_best_only=False),
+    EarlyStopping(patience=8, restore_best_weights=True),
+    ReduceLROnPlateau(patience=4, factor=0.1)
 ]
-
-metric = [metrics.Accuracy(), metrics.Precision(), metrics.Recall()]
 
 model.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
-    metrics=metric
+    metrics='accuracy'
 )
 
+batch = 64
+train = dataset.train(batch)
+val = dataset.val(batch)
+test = dataset.test(batch)
+
 model.fit(
-    dataset.train(128),
-    batch_size=128,
-    epochs=50,
-    validation_data=dataset.val(128),
+    train,
+    batch_size=batch,
+    epochs=38,
+    validation_data=val,
     callbacks=callbacks,
     class_weight=dataset.get_class_weights(),
 )
+
+model.evaluate(test, batch_size=batch)
+
+model.save(f'models/{model.name}')
